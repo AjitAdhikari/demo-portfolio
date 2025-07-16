d6784bb060eedff3eb9087850053fa60cb971062
Ajit Adhikari <ajit07adhikari@gmail.com>
2025-07-16 17:45:41 +0545

    add: header and portfolio image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
             <div className='text-xl font-bold'>Ajit Adhikari</div>
             <nav className='space-x-4'>
                 <Link href='/' className='hover:text-gray-400'>Home</Link>
-                <Link href='/about' className='hover:text-gray-400'>About</Link>
-                <Link href='/contact' className='hover:text-gray-400'>Contact</Link>
+                <Link href='/' className='hover:text-gray-400'>About</Link>
+                <Link href='/' className='hover:text-gray-400'>Contact</Link>
             </nav>
         </header>
     )
