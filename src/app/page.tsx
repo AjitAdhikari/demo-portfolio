f36a8f44e97fd13e22d3b2120c7e5adda9327d95
Ajit Adhikari <ajit07adhikari@gmail.com>
2025-07-16 17:44:06 +0545

    add: added banner.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import About from '@/components/about';
+import Banner from '@/components/banner';
 
 export default function Home() {
   return (
     <>
+    <title>My Portfolio</title>
       <Header />
-
+      <Banner />
+      <About />
       <Footer />
 
     </>
