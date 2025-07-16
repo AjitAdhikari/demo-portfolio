315d942d7200413ada75276bceeaea771e7eca43
Ajit Adhikari <ajit07adhikari@gmail.com>
2025-07-16 17:45:16 +0545

    add:  added banner.tsx

---
+++ src/components/banner.tsx
@@ -0,0 +1,12 @@
+export default function Banner() {
+    return (
+        <>
+        <section id="home" className="bg-blue-400 text-white text-center p-10">
+            <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio </h1>
+            <p className="mt-2 text-lg">I am Ajit Adhikari, a passionate web developer.</p>
+
+        </section>
+
+        </>
+    )
+}
